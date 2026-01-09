93bc2f13bf72db2f43abef070a2af40fdebfa3bf
Lucky Goyal <122284578+goyalucky@users.noreply.github.com>
2026-01-09 13:07:42 +0530

    main file for all the programs

---
+++ Queues/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		CallCenterManager cc = new CallCenterManager();
+		cc.Receive(new SupportCall());
+		cc.Receive(new SalesCall());
+		cc.Process();
+
+		/*TaskQueue tq = new TaskQueue();
+		tq.Add(new UploadTask());
+		tq.Add(new ReportTask());
+		tq.Process();*/
+
+		/*TicketService ts = new TicketService();
+		ts.Add(new BusTicket());
+		ts.Add(new TrainTicket());
+		ts.Add(new FlightTicket());
+		ts.Process();*/
+
+		/* PrintQueue pq = new PrintQueue();
+		pq.Add(new TextDocument());
+		pq.Add(new ImageFile());
+		pq.Process(); */
+	}
+}
+
